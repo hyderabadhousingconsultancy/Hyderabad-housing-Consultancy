91c08101e3e1c0c97446d32f63a21a282b788523
hyderabadhousingconsultancy <hyderabadhousingconsultancy@gmail.com>
2025-09-22 01:04:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs'); // Set EJS as the view engine
 app.set('views', path.join(__dirname, 'views')); // Specify views directory
